1bc6991c5d10611f1777f28b2dadfa6bed44be12
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2015-10-10 12:30:06 +0200

    fix commentary

--- cargo.el
+++ cargo.el
@@ -26,13 +26,14 @@
 ;; Current supported Cargo Key Combinations:
 ;;  * C-c C-c C-e - cargo-process-bench
 ;;  * C-c C-c C-b - cargo-process-build
-;;  * C-c C-c C-c - cargo-process-clean
+;;  * C-c C-c C-l - cargo-process-clean
 ;;  * C-c C-c C-d - cargo-process-doc
 ;;  * C-c C-c C-n - cargo-process-new
 ;;  * C-c C-c C-r - cargo-process-run
 ;;  * C-c C-c C-s - cargo-process-search
 ;;  * C-c C-c C-t - cargo-process-test
 ;;  * C-c C-c C-u - cargo-process-update
+;;  * C-c C-c C-c - cargo-process-repeat
 ;;
 ;;; Code:
 
